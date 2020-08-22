169473d5e1e625647c8bcd39e93cad0bd7e5e86d
Nana Adjei Manu <claeusdev@gmail.com>
2020-08-22 21:47:25 +0000

    make changes

--- app/src/Firebase.js
+++ app/src/Firebase.js
@@ -4,22 +4,21 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
+console.log(firebaseConfig.apiKey)
 // Initialize Firebase
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
-// const db = firebase.firestore();
-// const store = firebase.storage();
-// const auth = firebase.auth();
+export const db = firebase.firestore();
+export const store = firebase.storage();
+export const auth = firebase.auth();
 
-export default firebaseConfig;
+export default firebase;
